b9cc840b42689ffa2388ee940769f133736206a9
Fayaz <fayazbasha410@gmail.com>
2026-08-04 14:36:33 +0400

    feat: test configs + red team changes

--- playwright.config.js
+++ playwright.config.js
@@ -1,13 +1,15 @@
 import { defineConfig, devices } from '@playwright/test';
 
 
+
+
 export default defineConfig({
   testDir: './tests/specs',
   timeout: 120000,
   retries: 2,
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
-  workers: 1,
+  workers: process.env.CI ? 3 : 1,
   reporter: [['html', { open: 'never' }], ['list']],
   use: {
     baseURL: 'http://localhost:3000',
